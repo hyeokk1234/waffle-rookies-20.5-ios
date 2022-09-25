f54cce4f65af0f1f98327be55d21e3d76f348172
qqq981130 <qqq981130@gmail.com>
2022-09-25 22:56:24 +0900

    Alamofire 설치 완료

---
+++ seminar-2-assignment/seminar-2-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar-2-assignment
+//
+//  Created by 최성혁 on 2022/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
