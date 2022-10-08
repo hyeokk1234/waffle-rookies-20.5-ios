989d128deeb4f5a720a7ddd6ba5ea801127b7b2c
qqq981130 <qqq981130@gmail.com>
2022-10-08 13:52:09 +0900

    프로젝트 파일 생성, rxswift alamofire 설치 완료

---
+++ seminar-3-assignment/Pods/Target Support Files/Pods-seminar-3-assignment/Pods-seminar-3-assignment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_seminar_3_assignment : NSObject
+@end
+@implementation PodsDummy_Pods_seminar_3_assignment
+@end
---
+++ seminar-3-assignment/seminar-3-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar-3-assignment
+//
+//  Created by 최성혁 on 2022/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
