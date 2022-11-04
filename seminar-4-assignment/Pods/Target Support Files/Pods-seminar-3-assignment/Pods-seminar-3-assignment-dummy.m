e0ef7278f6b7b0b25d808461ae8199ca615c1c58
최성혁 <csh@choeseonghyeog-ui-MacBookAir.local>
2022-11-04 13:57:50 +0900

    Project 명 변경. seminar-4-assignment로

--- seminar-4-assignment/Pods/Target Support Files/Pods-seminar-3-assignment/Pods-seminar-3-assignment-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_seminar_3_assignment : NSObject
-@end
-@implementation PodsDummy_Pods_seminar_3_assignment
-@end
--- seminar-4-assignment/Pods/Target Support Files/Pods-seminar-4-assignment/Pods-seminar-4-assignment-umbrella.h
+++ seminar-4-assignment/Pods/Target Support Files/Pods-seminar-4-assignment/Pods-seminar-4-assignment-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_seminar_3_assignmentVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_seminar_3_assignmentVersionString[];
+FOUNDATION_EXPORT double Pods_seminar_4_assignmentVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_seminar_4_assignmentVersionString[];
 
