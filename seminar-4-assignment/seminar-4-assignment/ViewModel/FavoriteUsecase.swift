4bb1414d9533857ca1d57058b1a8c5cd1e25fb5a
최성혁 <qqq981130@gmail.com>
2022-11-08 13:53:35 +0900

    FavoriteUsecase 생성

--- seminar-4-assignment/seminar-4-assignment/ViewModel/FavoriteUsecase.swift
+++ seminar-4-assignment/seminar-4-assignment/ViewModel/FavoriteUsecase.swift
@@ -11,5 +11,27 @@ import RxCocoa
 import RxSwift
 
 class FavoriteUsecase {
+    var favorites: [MovieModel]
     
+    init() {
+        if let objects = UserDefaults.standard.value(forKey: "favorites") as? Data {
+            let decoder = JSONDecoder()
+            if let favoritesDecoded = try? decoder.decode(Array.self, from: objects) as [MovieModel] {
+                favorites = favoritesDecoded
+            } else {
+                favorites = [MovieModel]()
+            }
+        } else {
+            favorites = [MovieModel]()
+        }
+        
+        let favoriteObservable = Observable<[MovieModel]>
+            .create { emitter in
+                emitter.onNext(self.favorites)
+            
+                return Disposables.create()
+            }
+        
+//        _ = favoriteObservable.bind(to: favoritesSubject)
+    }
 }
